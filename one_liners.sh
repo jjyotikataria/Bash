ffdc07fd24e9c39a0887a3dcf44bee4935b57028
Jyoti Kataria <jyotikataria2008@gmail.com>
2021-05-18 20:22:53 +0530

    Update one_liners.sh

--- one_liners.sh
+++ one_liners.sh
@@ -1,3 +1,8 @@
 ## Read a file and print its contents linewise
 
 while read -r line; do echo "$line"; done < com.sh
+
+## Use arguments as inputs in the script ## $1 = pid (without P)  ## $2 = Path till the TOBESHARED (inside things will be uploaded to the FTP)
+
+perl upload.pl --projectid $1 --folder $2/'*' --loadmd FTP > dataupload.log &
+
