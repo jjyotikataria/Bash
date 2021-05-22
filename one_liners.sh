5fbf9fbb8407a36e8c6bb811581f6399c5f6daa6
Jyoti Kataria <jyotikataria2008@gmail.com>
2021-05-22 15:51:36 +0530

    Update one_liners.sh

--- one_liners.sh
+++ one_liners.sh
@@ -6,3 +6,8 @@ while read -r line; do echo "$line"; done < com.sh
 
 perl upload.pl --projectid $1 --folder $2/'*' --loadmd FTP > dataupload.log &
 
+## Do specific action for example wc -l for a a certain set of files
+
+for i in $(ls -lrth *A3SS* | awk '{print $9}') ; do  wc -l $i ; done
+
+##
